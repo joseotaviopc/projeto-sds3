569497a678216fe808371893c0e34d2e7bee2d68
joseotaviopc <carvalho.jose@gmail.com>
2021-09-11 10:59:14 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,8 @@
-import BarChart from "components/BarChart";
-import DonutChart from "components/DonutChart";
-import DataTable from "components/DataTable";
-import Footer from "components/Footer";
-import NavBar from "components/NavBar";
-import React from "react";
+import Routes from "Routes";
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="container">
-        <h3 className="py-3 text-primary">Dashboard</h3>
-        <div className="row px-3 py-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Receita(caixa)</h5>
-            <BarChart />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Receita(trimestral)</h5>
-            <DonutChart />
-          </div>  
-        </div>
-        <div className="py-3">
-          <h4 className="text-secondary">Geração de Caixa</h4>
-        </div>
-        <DataTable />
-      </div>
-      <Footer />
-    </>
+    <Routes />
   );
 }
 
---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,33 @@
+import BarChart from "components/BarChart";
+import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
+function Dashboard() {
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <h3 className="py-3 text-primary">Dashboard</h3>
+        <div className="row px-3 py-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Receita(caixa)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Receita(trimestral)</h5>
+            <DonutChart />
+          </div>
+        </div>
+        <div className="py-3">
+          <h4 className="text-secondary">Geração de Caixa</h4>
+        </div>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Dashboard;
